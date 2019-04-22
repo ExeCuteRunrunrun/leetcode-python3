8505d10ae9fdcae7853a383c1d77d5f98fc1c436
Manying ZHANG <manying.zhang.execute@gmail.com>
2019-04-22 21:50:18 +0200

    Create jewels and stones.py

---
+++ jewels and stones.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        n=0
+        for i in S:
+            if i in J:
+                n+=1
+                
+        return n
