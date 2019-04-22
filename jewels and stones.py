5fbcc514f6d40529db016c4df23c664c48305e63
Manying ZHANG <manying.zhang.execute@gmail.com>
2019-04-22 21:53:34 +0200

    Update jewels and stones.py

--- jewels and stones.py
+++ jewels and stones.py
@@ -1,8 +1,10 @@
 class Solution:
+    # to find how many letters in string S (for stone) are letters in string J (for jewel)
+    # input J, S
+    # output n (number of common letters)
     def numJewelsInStones(self, J: str, S: str) -> int:
         n=0
         for i in S:
             if i in J:
-                n+=1
-                
+                n+=1        
         return n
